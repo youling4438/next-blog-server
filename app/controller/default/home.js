75d31242af2df2015a83792c33a2269580219715
youling4438 <youling0106@163.com>
2020-09-15 22:44:53 +0800

    sql design and my first api interface

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -10,7 +10,18 @@ class HomeController extends Controller {
 	} 
 	async home() {
 		this.ctx.body = "<h2>Hello Home!</h2>"
-	} 
+	}
+	async getArticleList() {
+		let sql = 'SELECT article.id as id ,' + 
+		'article.title as title ,' + 
+		'article.introduce as intro ,' + 
+		'article.addTime as addTime ,' + 
+		'article.view_count as viewCount ,' + 
+		'type.typeName as typeName ' + 
+		'FROM article LEFT JOIN type ON article.type_id = type.id';
+		const results = await this.app.mysql.query(sql);
+		this.ctx.body = {data: results};
+	}
 }
 
 module.exports = HomeController;
--- app/router/default.js
+++ app/router/default.js
@@ -3,5 +3,6 @@
 module.exports = app => {
 	const {router,controller} = app
     router.get('/default/index',controller.default.home.index),
-    router.get('/default/home',controller.default.home.home)
+    router.get('/default/home',controller.default.home.home),
+    router.get('/getArticleList',controller.default.home.getArticleList)
 }
